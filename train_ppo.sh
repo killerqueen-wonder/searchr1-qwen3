5f991ce2523f664f9533b489ee5b4a72aa48004a
NEVVY <nevvy@qq.com>
2025-11-01 15:59:22 +0800

    max_num_batched_tokens

--- train_ppo.sh
+++ train_ppo.sh
@@ -57,6 +57,7 @@ PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=true \
     actor_rollout_ref.rollout.log_prob_micro_batch_size=32 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.rollout.max_num_batched_tokens=8500 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.actor.use_kl_loss=true \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
