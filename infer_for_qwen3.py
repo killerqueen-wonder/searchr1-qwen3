308a44f8713b68924d4a94466bc3a13d5d92928f
NEVVY <nevvy@qq.com>
2025-11-11 00:29:21 +0800

    1

--- infer_for_qwen3.py
+++ infer_for_qwen3.py
@@ -181,7 +181,7 @@ if __name__ == '__main__':
 
         generated_tokens = outputs[0][input_ids.shape[1]:]
         output_text = tokenizer.decode(generated_tokens, skip_special_tokens=True)
-        # print(f'[debug] output "{output_text}"...')
+        print(f'[debug] output "{output_text}"...')
         tmp_query = get_query(tokenizer.decode(outputs[0], skip_special_tokens=True))
         if tmp_query:
             
