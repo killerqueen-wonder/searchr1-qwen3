4f8d5f52fe4878ef0a82fae6c38cf6b01c72b821
NEVVY <nevvy@qq.com>
2025-11-19 22:44:05 +0800

    1

--- test_requests.py
+++ test_requests.py
@@ -79,7 +79,7 @@ def search(retrieve_path, payload):
             text = "\n".join(content.split("\n")[1:])
             
             score=doc_item['score']
-            score=(round(float(score), 2))
+            score=(round(float(score), 4))
             
             format_reference += f"Doc {idx+1}(Title: {title}) {text}\n score={score}\n"
         return format_reference
