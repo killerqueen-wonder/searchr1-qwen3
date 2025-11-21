af62e7aee84349390231b3011e22e3c7cebea480
NEVVY <nevvy@qq.com>
2025-11-21 14:28:49 +0800

    1

--- search_r1/search/retrieval_server_text2vec.py
+++ search_r1/search/retrieval_server_text2vec.py
@@ -183,7 +183,7 @@ class BM25Retriever(BaseRetriever):#rank bm25+jieba
 
         # 对语料库进行分词与预处理
         self.docs_raw = [doc["contents"] for doc in self.corpus]
-        self.docs_tokenized = [list(lawa.cut_for_search(text)) for text in self.docs_raw]
+        self.docs_tokenized = [list(lawa.cut(text)) for text in self.docs_raw]
 
         # 构建 BM25
         self.bm25 = BM25Okapi(self.docs_tokenized,k1=1.5, b=0.5)
