3a6cdc715e6810843b5abd2476223a75c9ce68f6
NEVVY <nevvy@qq.com>
2025-12-05 21:30:26 +0800

    1

--- test_requests.py
+++ test_requests.py
@@ -55,7 +55,7 @@ def search(retrieve_path, payload):
         )
         response.raise_for_status()
         json_data = response.json()
-        print(json_data)
+        print(f'[debug]response:{json_data}')
         results = json_data.get("result", [])
     except requests.exceptions.Timeout:
         print("[ERROR] Search request timed out.")
