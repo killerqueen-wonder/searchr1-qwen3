0f33cf55641b7d54da563af9359a45d8bad877ef
NEVVY <nevvy@qq.com>
2026-02-07 15:51:57 +0800

    1

--- train_grpo_RL6.0.sh
+++ train_grpo_RL6.0.sh
@@ -48,7 +48,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.max_num_batched_tokens=23000 \
     algorithm.use_kl_in_reward=false \
     +algorithm.no_think_rl=false \
-    trainer.use_critic=false \
+    +trainer.use_critic=false \
     trainer.do_search=true \
     trainer.critic_warmup=0 \
     trainer.logger='["wandb"]' \
