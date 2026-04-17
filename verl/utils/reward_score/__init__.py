050e3976cd28216d7a5d4f1286d3fe7d708bfa34
NEVVY <nevvy@qq.com>
2026-04-17 14:46:52 +0800

    1

--- verl/utils/reward_score/__init__.py
+++ verl/utils/reward_score/__init__.py
@@ -114,7 +114,7 @@ def default_compute_score(
     ]:
         from . import local_LLM_judgement_grpo
 
-        res = local_LLM_judgement_grop.compute_score(solution_str, ground_truth,extra_info)
+        res = local_LLM_judgement_grpo.compute_score(solution_str, ground_truth,extra_info)
     elif data_source in [
         'legal_exam',
     ]:
