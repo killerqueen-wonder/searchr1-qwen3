caad0c664ffebc0cfa7da982f1bc7108af5cb7bf
NEVVY <nevvy@qq.com>
2026-04-27 15:58:37 +0800

    RL7.2

--- bench/run_bench.sh
+++ bench/run_bench.sh
@@ -4,10 +4,10 @@ set -e
 # ================= 1. 配置全局变量 =================
 # 允许外部单行命令传参覆盖
 BASE_DIR="${BASE_DIR:-/data/panghuaiwen/legal_R1}"
-MODEL_PATH="${MODEL_PATH:-${BASE_DIR}/model/RL_ckp/legal_exam-ppo-qwen3-8b-RL-7.1-0417/global_step_800/actor_merge}"
-MODEL_NAME="${MODEL_NAME:-RL_7.1_0425}"
+MODEL_PATH="/data/panghuaiwen/legal_R1/model/RL_ckp/legal_exam-ppo-qwen3-8b-RL-7.2-0420/global_step_700/actor_merge"
+MODEL_NAME="${MODEL_NAME:-RL_7.2_0427}"
 JUDGE_MODEL_NAME="Qwen3-8B-Judge" # 为裁判模型指定独立名称
-WORKERS="${WORKERS:-32}"
+WORKERS="${WORKERS:-48}"
 
 # 接口与并发配置
 RETRIEVE_PATH="http://127.0.0.1:8005/retrieve" 
