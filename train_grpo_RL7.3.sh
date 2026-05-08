a98047bfe4cf1037aac9f5179a1e791b213b1b02
NEVVY <nevvy@qq.com>
2026-05-08 13:53:25 +0800

    1

--- train_grpo_RL7.3.sh
+++ train_grpo_RL7.3.sh
@@ -177,9 +177,9 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.35 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.25 \
     actor_rollout_ref.rollout.n=4 \
-    +actor_rollout_ref.rollout.engine_kwargs.vllm.quantization=fp8 \
+    +actor_rollout_ref.rollout.engine_kwargs.vllm.kv_cache_dtype='fp8_e5m2' \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.ref.fsdp_config.param_offload=true \
     actor_rollout_ref.rollout.temperature=0.4 \
