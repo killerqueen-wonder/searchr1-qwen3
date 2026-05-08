3e75a81180e50121713571ee952aaa69a0135cc9
NEVVY <nevvy@qq.com>
2026-05-08 21:52:23 +0800

    1

--- train_grpo_RL7.35.sh
+++ train_grpo_RL7.35.sh
@@ -184,7 +184,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.35 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.3 \
     actor_rollout_ref.rollout.enforce_eager=true \
     actor_rollout_ref.rollout.n=2 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
