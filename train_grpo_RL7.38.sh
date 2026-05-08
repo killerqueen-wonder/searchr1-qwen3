dfcf2e612b9b8cb22f8c620eb45d1d1c209003a9
NEVVY <nevvy@qq.com>
2026-05-09 01:34:08 +0800

    RL7.38

--- train_grpo_RL7.38.sh
+++ train_grpo_RL7.38.sh
@@ -3,7 +3,7 @@
 # RAG + LLM Judge + GRPO 全自动训练总管道脚本 (RL 7.35)
 # 适用环境：4 x A800 (80GB), Kubernetes / AI Studio
 # 显存隔离方案 FSDP + TP=2 + FlashAttention + FP8(后台)
-#可以运行但极慢，24min一步，而且每卡占用20G
+
 # ==============================================================================
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3
@@ -156,8 +156,8 @@ python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=$DATA_DIR/train.parquet \
     data.val_files=$DATA_DIR/test.parquet \
-    data.train_batch_size=8 \
-    data.val_batch_size=8 \
+    data.train_batch_size=32 \
+    data.val_batch_size=32 \
     data.max_prompt_length=28000 \
     data.max_response_length=1200 \
     +data.max_start_length=4000 \
@@ -169,8 +169,8 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.lora_rank=64 \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=true \
-    actor_rollout_ref.actor.ppo_mini_batch_size=8 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=16 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.actor.use_kl_loss=true \
     actor_rollout_ref.actor.kl_loss_coef=0.06 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
@@ -179,16 +179,16 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.ref.fsdp_config.model_dtype=bfloat16 \
     actor_rollout_ref.model.enable_gradient_checkpointing=true \
     actor_rollout_ref.model.use_fused_kernels=true \
-    actor_rollout_ref.actor.fsdp_config.param_offload=true \
-    actor_rollout_ref.actor.fsdp_config.optimizer_offload=true \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
+    actor_rollout_ref.actor.fsdp_config.param_offload=false \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=false \
+    actor_rollout_ref.ref.fsdp_config.param_offload=false \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.3 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
     actor_rollout_ref.rollout.enforce_eager=true \
     actor_rollout_ref.rollout.n=4 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
-    actor_rollout_ref.ref.fsdp_config.param_offload=true \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.rollout.temperature=0.4 \
     actor_rollout_ref.rollout.max_num_batched_tokens=32000 \
     algorithm.use_kl_in_reward=false \
