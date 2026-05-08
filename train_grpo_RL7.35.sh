0ba035a8f46f59c63d5c00f76edf9833813f0ede
NEVVY <nevvy@qq.com>
2026-05-08 19:56:09 +0800

    1

--- train_grpo_RL7.35.sh
+++ train_grpo_RL7.35.sh
@@ -206,7 +206,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.resume_mode=disable \
     trainer.default_local_dir=/F00120250029/lixiang_share/panghuaiwen_share/legal_R1/model/RL_ckp/$EXPERIMENT_NAME \
     +max_turns=10 \
-    ray_kwargs.ray_init.num_gpus=2 \
+    ray_kwargs.ray_init.num_cpus=16 \
     +retriever.url="http://127.0.0.1:8005/retrieve" \
     +retriever.topk=8 \
     2>&1 | tee $EXPERIMENT_NAME.log
