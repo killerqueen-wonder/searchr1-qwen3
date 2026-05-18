fd1cea30b91c596bb23e840c0ce660ecb5a72fac
NEVVY <nevvy@qq.com>
2026-05-18 08:43:47 +0800

    1

--- reward_model_test.py
+++ reward_model_test.py
@@ -6,7 +6,7 @@ from vllm import LLM, SamplingParams
 
 # ================= 配置区 =================
 # 模型与数据路径 (请根据实际情况修改)
-MODEL_PATH = "/F00120250029/lixiang_share/panghuaiwen_share/legal_R1/model/SFT_ckp/your-sft-model-path"
+MODEL_PATH = "/F00120250029/lixiang_share/panghuaiwen_share/legal_R1/model/SFT_ckp/qwen3_reward_model_0517/checkpoint-2-2172/tfmr"
 TEST_JSON_PATH = "/F00120250029/lixiang_share/panghuaiwen_share/legal_R1/dataset/dataset/reward_model_training/test.json"
 OUTPUT_RESULT_PATH = "/F00120250029/lixiang_share/panghuaiwen_share/legal_R1/dataset/dataset/reward_model_training/eval_results_compare.json"
 
